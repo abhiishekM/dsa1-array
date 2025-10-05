42994ac2eb3e9068f503db0754f9c307b8944938
Abhishek Mishra <95407551+abhishivam1@users.noreply.github.com>
2025-10-06 00:02:47 +0530

    Update programs.c

--- programs.c
+++ programs.c
@@ -8,7 +8,7 @@ int main() {
     int arr[n];
 
     printf("Enter %d elements:\n", n);
-//trial only trial.hehe
+//trial only trial.hehefjdj
     for(i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
